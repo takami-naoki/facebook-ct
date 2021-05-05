175a1d5c3ae960e8fbf44a9ce01b7e8da5a369f2
MacMini2 <macmini2@mac-mini.ad.creativepace.com>
2021-05-05 13:01:44 -0700

    initial commit

---
+++ resources/js/components/Start.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>sfdfdfd</div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/posts', 'PostController@index');
+    Route::post('/posts', 'PostController@store');
+});
